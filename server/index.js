cd215da1cb8ae95f9866ba349db9e99360a59c44
mahume <michael.a.hume@gmail.com>
2019-10-07 19:32:36 -0600

    Add: Passport configuration

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 // New Running Express Application
@@ -7,10 +8,17 @@ const app = express();
 const passport = require('passport');
 // Setup Google Strategy for Passport.js
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-passport.use(new GoogleStrategy());
-
-
-
+passport
+  .use(new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    }, 
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  ))
 
 // Dynamic Port for Heroku
 const PORT = process.env.PORT || 3000;
