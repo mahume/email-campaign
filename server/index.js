dab91998800945b7a73d6779970856308d561192
mahume <michael.a.hume@gmail.com>
2019-10-07 23:43:48 -0600

    Add: mongoose.connect

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Shorter syntax for running a file. No need to extract and use as variable
 require('./services/passport');
+
+mongoose.connect(process.env.MONGODB_URI);
 
 // New Running Express Application
 const app = express();
