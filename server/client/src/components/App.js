cd9e6b9f5c46aee6f6f475301b748bbc57898276
mahume <michael.a.hume@gmail.com>
2019-10-10 16:56:14 -0600

    Add: React Router setup

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return(
-    <h1>Hello World</h1>
+    <div>
+      <BrowserRouter>
+        <Fragment>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </Fragment>
+      </BrowserRouter>
+    </div>
   )
 }
 
