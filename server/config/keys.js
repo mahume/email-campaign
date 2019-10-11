154445fca80753bb2d35f51185dc5e6c6653fb0b
mahume <michael.a.hume@gmail.com>
2019-10-11 17:17:19 -0600

    Changed logic to switch case

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,9 @@
 // Figure out which set of credentials to return
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod');
-} else {
-  module.exports = require('./dev');
+switch (process.env.NODE_ENV) {
+  case 'production':
+    module.exports = require('./prod');
+    break;
+  default:
+    module.exports = require('./dev');
+    break;
 }
