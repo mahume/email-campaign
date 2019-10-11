a4f18e383691d2ebd2f280601bf4ded9e354564d
mahume <michael.a.hume@gmail.com>
2019-10-10 18:27:05 -0600

    Dynamic Navbar based on logged in status complete

--- server/client/src/components/Header/index.js
+++ server/client/src/components/Header/index.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 import { Navbar, Logo, NavList, NavLink } from './styles';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <NavLink href="/auth/google">Login With Google</NavLink>
+      default:
+        return <NavLink href="/api/logout">Logout</NavLink>
+    }
+  }
   render() {
     return (
       <Navbar>
           <Logo>Email-campaign</Logo>
           <NavList>
             <li>
-              <NavLink href="">Login With Google</NavLink>
+              {this.renderContent()}
             </li>
           </NavList>
       </Navbar>
@@ -16,4 +27,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
