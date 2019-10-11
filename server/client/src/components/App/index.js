341ae55abe017789ffe85d31d383164d3aec0cd8
mahume <michael.a.hume@gmail.com>
2019-10-10 18:00:59 -0600

    Add: Redux Thunk.

--- server/client/src/components/App/index.js
+++ server/client/src/components/App/index.js
@@ -1,6 +1,7 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 import { GlobalStyle } from './styles';
 
 import Header from '../Header/index';
@@ -8,20 +9,27 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-  return(
-    <div>
-      <BrowserRouter>
-        <Fragment>
-          <GlobalStyle />
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </Fragment>
-      </BrowserRouter>
-    </div>
-  )
+class App extends Component {
+  
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return(
+      <div>
+        <BrowserRouter>
+          <Fragment>
+            <GlobalStyle />
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </Fragment>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect(null, actions)(App);
