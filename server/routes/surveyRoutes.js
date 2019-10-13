13bda911782d157a14259c6f64d6ea3b9f95d644
mahume <michael.a.hume@gmail.com>
2019-10-12 18:51:07 -0600

    Adding redirect route

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -8,6 +8,10 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!');
+  })
+  
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
     const survey = new Survey({
@@ -22,6 +26,14 @@ module.exports = app => {
     })
     // Send an Email
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    await mailer.send();
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (error) {
+      res.status(422).send(error);
+    }
   })
 }
